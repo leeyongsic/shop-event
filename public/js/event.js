c1338163a4f970e51dfd597ee29c8f022d185696
hiphoper-hansm <hansm@hiphoper.com>
2019-03-18 20:51:35 +0900

    [PC] 0315 YAN13 추가

--- public/js/event.js
+++ public/js/event.js
@@ -20,21 +20,21 @@
 			autoplay:true,
 			autoplaySpeed:5000,
 			pauseOnDotsHover:true,
-			slidesToShow: 3,
+			slidesToShow: 2,
 			slidesToScroll: 1,
-			centerMode: true,
+			centerMode: false,
 			centerPadding: '10',
 			variableWidth: true,
 			respondTo : 'window',
 			responsive: [
 				{breakpoint:3000,
-				settings: {slidesToShow:3,slidesToScroll:1}
+				settings: {slidesToShow:2,slidesToScroll:1}
 				},
 				{breakpoint:1160,
-				settings: {slidesToShow:4,slidesToScroll:1}
+				settings: {slidesToShow:2,slidesToScroll:1}
 				},
 				{breakpoint:1024,
-				settings: {slidesToShow:4,slidesToScroll:2}
+				settings: {slidesToShow:2,slidesToScroll:2}
 				}
 			]
 		}).each(function(i,el){
