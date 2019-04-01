44c669a60751b4a91ba3528821b68de652c00841
hiphoper-hansm <hansm@hiphoper.com>
2019-04-01 10:08:43 +0900

    앤더슨벨 스크립트 및 css 수정 적용

--- public/js/event.js
+++ public/js/event.js
@@ -182,7 +182,7 @@
 			autoplay:true,
 			autoplaySpeed:5000,
 			pauseOnDotsHover:true,
-			slidesToShow: 2,
+			slidesToShow: 1,
 			slidesToScroll: 1,
 			centerMode: false,
 			centerPadding: '10',
