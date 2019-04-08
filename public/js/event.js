4b5eab1d751e5589634594f3bf7996ff491145a8
hiphoper-hansm <hansm@hiphoper.com>
2019-04-08 10:57:29 +0900

    modify

--- public/js/event.js
+++ public/js/event.js
@@ -386,10 +386,12 @@
 			$cont_tip_toggle = $('.cont_tip_toggle');
 			
 		$btn_tip_toggle.on('click', function(e) {
-			//e.preventDefault();
+			e.preventDefault();
 
 			$(this).toggleClass('on');
 			$(this).parent().find('.cont_tip_toggle').toggle();
+
+			console.log(this);
 
 		});
 
